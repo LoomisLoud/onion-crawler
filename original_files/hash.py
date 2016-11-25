819a6231396a1a9b74aa626f555a09d1792af203
rbsteinm <raphael.steinmann@epfl.ch>
2016-11-25 13:38:07 +0100

    add files provided by Sophie in the repo

---
+++ original_files/hash.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# coding: utf-8
+#authors: Pineau T. & Schopfer A.
+
+import urllib2
+import hashlib
+
+def sourceCode(url):
+    """Récupération du code source d'une URL"""
+    headers = {'user-agent': "Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0"}
+    request = urllib2.Request(url, headers=headers)
+    contents = urllib2.urlopen(request).read()
+    return contents
+
+def saveContent(data, filename):
+    """Enregistrement de data dans un fichier filename"""
+    with open(filename, 'wb+') as f:
+        f.write(data)
+
+def hasher(data):
+    """fonction de hashage: md5"""
+    h = hashlib.md5()
+    h.update(data)
+    return h.hexdigest()
+
+   
+s=sourceCode("http://ipsac.unil.ch/traces/")
+print 'code source: ' +hasher(s)
+saveContent(s, 'test.html')
+
+"""
+with open('test.html', 'rb') as f:
+    buf = f.read()
+print 'contenu du fichier: ' + hasher(buf)
+"""
---
+++ original_files/source_code.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# coding: utf-8
+#authors: Pineau T. & Schopfer A.
+
+import urllib2
+import requests #pip install requests
+
+    
+def requestsSC(url):
+    """Récupération du code source d'une URL via requests"""
+    headers = {'user-agent': "Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0"}
+    r = requests.get(url, headers=headers)
+    contents = r.content
+    return contents
+
+def urllibSC(url):
+    """Récupération du code source d'une URL via urllib2"""
+    headers = {'user-agent': "Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0"}
+    request = urllib2.Request(url, headers=headers)
+    contents = urllib2.urlopen(request).read()
+    return contents
+
+def saveContent(data, filename):
+    """Enregistrement de data dans un fichier filename"""
+    with open(filename, 'wb+') as f:
+        f.write(data)
+    print 'done'
+
+s=requestsSC("http://ipsac.unil.ch/traces/")
+#s=urllibSC("http://ipsac.unil.ch/traces/")
+
+print s
+
+
+#saveContent(s, 'test.html')
